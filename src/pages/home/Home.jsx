818e053b53a75135e1e44c357385fb4c406b0c38
Ali <aliuajibade317@gmail.com>
2024-10-30 10:45:16 -0700

    add sidebar icons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Topbar from '../../components/topbar/Topbar'
+import "./home.css";
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Feed from '../../components/feed/Feed';
+
 
 const Home = () => {
   return (
     <>
         <Topbar />
+        <div className="home">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </div>
     </>
   )
 }
