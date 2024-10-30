90b5eec5bab4f4d23139f01996fe5261da1324b2
Ali <aliuajibade317@gmail.com>
2024-10-30 15:15:02 -0700

    share component

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,8 +1,25 @@
+import { PermMedia } from "@mui/icons-material";
 import "./share.css";
 
 const Share = () => {
   return (
-    <div>Share</div>
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img className="shareProfileImg" src="./assets/person/11.jpg" alt="shareProfileImg" />
+          <input className="shareInput" placeholder="What's in your mind Ali?" />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMedia />
+              <span className="shareOptionText">Photo or Video</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
